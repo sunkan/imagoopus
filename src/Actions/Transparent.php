847088932c4082f2b9b1cce1e251c5af4700fc9f
Andreas Sundqvist <andreas@forme.se>
2015-02-26 14:11:43 +0100

    Change fuzz to .1 and .8

--- src/Actions/Transparent.php
+++ src/Actions/Transparent.php
@@ -8,14 +8,14 @@ class Transparent extends AAction {
 
     public function run(Image $image) {
         $target = $image->getImagePixelColor(1, 1);
-        $fuzz = 0.01;
+        $fuzz = 0.1;
         if ($target->getColorAsString()!='rgb(255,255,255)') {
             $colors = $image->getImageHistogram(2);
             $target = array_shift($colors);
-            $fuzz = 0.08;
+            $fuzz = 0.8;
         }
 
-        $image->transparentPaintImage($target, 0, $image->getQuantum()*$fuzz, false);
+        $image->transparentPaintImage($target, 0, $image->getQuantum()*$fuzz, true);
 
         return $image;
     }
