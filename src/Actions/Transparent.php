f20b8db2ed47e2f59fb3e0d451f9a974fd81d39b
Andreas Sundqvist <andreas@forme.se>
2015-02-26 13:09:04 +0100

    Add transparent action

---
+++ src/Actions/Transparent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ImagoOpus\Actions;
+
+use ImagoOpus\Image;
+
+class Transparent extends AAction {
+
+    public function run(Image $image) {
+        $target = $image->getImagePixelColor(1, 1);
+        $fuzz = 0.01;
+        if ($target->getColorAsString()!='rgb(255,255,255)') {
+            $colors = $image->getImageHistogram(2);
+            $target = array_shift($colors);
+            $fuzz = 0.08;
+        }
+
+        $image->transparentPaintImage($target, 0, $image->getQuantum()*$fuzz, false);
+
+        return $image;
+    }
+}
--- src/Image.php
+++ src/Image.php
@@ -6,8 +6,13 @@ use Imagick;
 
 class Image {
     protected $imagick;
+    protected $source;
     public function __construct($src) {
+        $this->source = $src;
         $this->imagick = new Imagick($src);
+    }
+    public function getSource() {
+        return $this->source;
     }
 
     public function getImagick() {
@@ -24,10 +29,46 @@ class Image {
             return true;
         }
     }
+    public function function getHexColor(\ImagickPixel $pixel) {
+        $color = $pixel->getColor();
+
+        return sprintf('#%s%s%s', 
+            dechex($color['r']), 
+            dechex($color['g']),
+            dechex($color['b'])
+        );
+    }
+    public function getImageHistogram($count=0) {
+        if (!$count) {
+            return $this->imagick->getImageHistogram();
+        }
+
+        exec('convert '.$image->get.' -colors '.(int)$count.' -format "%c" histogram:info:', $output);
+
+        $sort = [];
+        $data = [];
+
+        foreach($output as $i => $row) {
+            $rs = preg_match('/(\d+):\s*\(\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)\s*(\#[a-f0-9]{6})\s*rgb\(\d{1,3},\d{1,3},\d{1,3}\)/i', $row, $m);
+            if ($rs) {
+                $sort[$i] = (int)$m[1];
+                $data[$i] = new \ImagickPixel($m[5]);
+                $data[$i]->count = (int)$m[1];
+            }
+        }
+        arsort($sort);
+        $keys = array_flip($sort);
+        $return = [];
+        foreach ($keys as $key) {
+            $return[] = $data[$key];
+        }
+
+        return $return;
+    }
 
     public function getQuantum() {
         $quantum = $this->imagick->getQuantumRange();
-        return $quantum['quantumRangeLong'];
+        return pow(2, $quantum['quantumRangeLong']);
     }
     public function getRatio() {
         $dim = $this->imagick->getImageGeometry();
