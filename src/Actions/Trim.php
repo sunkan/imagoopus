746dbd8a705f10af28a19d7a15f47b345649a608
Andreas Sundqvist <andreas@forme.se>
2015-02-27 15:35:11 +0100

    Special condition if background not #fff

--- src/Actions/Trim.php
+++ src/Actions/Trim.php
@@ -7,6 +7,13 @@ use ImagoOpus\Image;
 class Trim extends AAction {
     public function run(Image $image) {
         $fuzz = $this->options['fuzz']?:0.1;
+        if ($fuzz == 0.1) {
+            $target = $image->getImagePixelColor(1, 1);
+            if ($target->getColorAsString()!='srgb(255,255,255)' && $target->getColorAsString()!='rgb(255,255,255)') {
+                $fuzz = 0.8;
+            }
+        }
+        
         $quantum = $image->getQuantum();
         $image->trimImage($fuzz*$quantum);
         return $image;
