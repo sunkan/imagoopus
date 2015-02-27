8f8b19d1b6278e4bf451679d29cf545743e04541
Andreas Sundqvist <andreas@forme.se>
2015-02-27 15:38:50 +0100

    Fixed fuzz factor and correct invert flag

--- src/Actions/Transparent.php
+++ src/Actions/Transparent.php
@@ -7,15 +7,16 @@ use ImagoOpus\Image;
 class Transparent extends AAction {
 
     public function run(Image $image) {
+        $image->setImageFormat('png');
         $target = $image->getImagePixelColor(1, 1);
-        $fuzz = 0.1;
-        if ($target->getColorAsString()!='rgb(255,255,255)') {
+        $fuzz = 0.01;
+        if ($target->getColorAsString()!='srgb(255,255,255)' && $target->getColorAsString()!='rgb(255,255,255)') {
             $colors = $image->getImageHistogram(2);
             $target = array_shift($colors);
-            $fuzz = 0.8;
+            $fuzz = 0.08;
         }
 
-        $image->transparentPaintImage($target, 0, $image->getQuantum()*$fuzz, true);
+        $image->transparentPaintImage($target, 0, $image->getQuantum()*$fuzz, false);
 
         return $image;
     }
