8207a2a680fba14278884572280b9455c80876a3
Andreas Sundqvist <andreas@forme.se>
2015-03-12 11:55:40 +0100

    Add compositeColor method Add baseCurve method Add levels method Add colortone method Add customVignette method

--- src/Image.php
+++ src/Image.php
@@ -104,4 +104,81 @@ class Image {
         }
         throw new BadMethodCallException("No method by that name");
     }
+
+    protected static $_channels = [
+        'red' => Imagick::CHANNEL_RED,
+        'green' => Imagick::CHANNEL_GREEN,
+        'blue' => Imagick::CHANNEL_BLUE,
+    ];
+    protected static $_composites = [
+        'overlay' => Imagick::COMPOSITE_OVERLAY,
+        'multiply' => Imagick::COMPOSITE_MULTIPLY
+    ];
+
+    public function compositeColor($color, $mode = 'normal') {
+        $mode = self::$_composites[$mode];
+        $layer = new Imagick();
+        $layer->newImage($this->imagick->getImageWidth(), $this->imagick->getImageHeight(), new ImagickPixel($color));
+        $layer->setImageFormat('jpg');
+        $this->imagick->compositeImage($layer, $mode, 0, 0);
+        return $this;
+    }
+    public function baseCurve($channel, $value, $output = false) {
+        $expression = 'u+';
+        $channel = self::$_channels[$channel];
+        if ($output == true) {
+            $expression .= '(' . (string) $value . '/255)';
+        } else {
+            $expression .= '(u*' . (string) $value . '/255)';
+        }
+        $this->imagick = $this->imagick->fxImage($expression, $channel);
+        return $this;
+    }
+    public function levels($black, $gamma, $white) {
+        $quantumRangeLong = $image->getQuantum();
+        $this->imagick->levelImage(
+            $black*$quantumRangeLong/255,
+            $gamma,
+            $white*$quantumRangeLong/255,
+            Imagick::CHANNEL_ALL
+        );
+        return $this;
+    }
+    public function colortone($color, $level, $type = 0) {
+        $this->imagick->setImageColorspace ( Imagick::COLORSPACE_RGB ); 
+        $args = [$level, 100-$level];
+        $negate = $type == 0 ? TRUE : FALSE;
+
+        $opacityColor = new ImagickPixel("rgba(0, 0, 0, 100)");
+        $layer_1 = clone $this->imagick;
+        $layer_1->colorizeImage($color, $opacityColor);
+
+        $layer_2 = clone = $this->imagick;
+        $layer_2->setImageColorspace( Imagick::COLORSPACE_GRAY );
+        if ($negate) {
+            $layer_2->negateImage(0);
+        }
+
+        $this->imagick->setOption( 'compose:args', $args[0] . ',' . $args[1] );
+
+        $this->imagick->compositeImage( $layer_1, Imagick::COMPOSITE_BLEND, 0, 0 );
+        $this->imagick->compositeImage( $layer_2, Imagick::COMPOSITE_BLEND, 0, 0 );
+    }
+    public function customVignette($color_1 = 'none', $color_2 = 'black', $crop_factor = 1.5)
+    {
+        $dim = $this->imagick->getImageGeometry();
+        $crop_x = floor($dim['width'] * $crop_factor);
+        $crop_y = floor($dim['height'] * $crop_factor);
+        
+        $layer = new Imagick();
+        $layer->newPseudoImage($crop_x, $crop_y,'radial-gradient:'.$color_1.'-'.$color_2);
+
+        $layer->cropThumbnailImage($dim['width'], $dim['height']);
+        $layer->setImagePage($dim['width'], $dim['height'], 0, 0);
+
+        $this->imagick->compositeImage( $layer_1, Imagick::COMPOSITE_MULTIPLY, 0, 0 );
+        $this->imagick->mergeImageLayers(Imagick::LAYERMETHOD_FLATTEN);
+
+        return $this;
+    }
 }
